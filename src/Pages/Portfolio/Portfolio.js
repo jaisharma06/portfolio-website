71e830b1a93e284ce7429bcbe46c13292f646540
Jai Prakash <jai@mplgaming.com>
2024-03-23 17:02:23 +0530

    added images for bg

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Portfolio.css";
 import IntroText from "../../Components/IntroText/IntroText";
+import backgroundImg from "../../content/boy_playing.webp";
 
 class Portfolio extends React.Component {
   render() {
     return (
       <div className="portfolio">
+        <img src={backgroundImg} className="contacts-bg" />
         <IntroText subHeading={["I want to create beautiful things."]}>
           Portfolio
         </IntroText>
