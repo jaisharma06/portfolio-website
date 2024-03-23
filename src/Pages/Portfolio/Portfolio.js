454c711ee4b094b70bd0b2490827e06c87697395
jaisharma06 <jai@mplgaming.com>
2024-03-23 18:18:45 +0530

    fixes

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -6,7 +6,12 @@ class Portfolio extends React.Component {
   render() {
     return (
       <div className="portfolio">
-        <IntroText subHeading={["I want to create beautiful things."]}>
+        <IntroText
+          subHeading={[
+            "I want to create beautiful things.",
+            "If you can program it, you can build it. If you can build it, you can make it do something worthwhile",
+          ]}
+        >
           Portfolio
         </IntroText>
       </div>
